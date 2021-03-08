360b4c5131a19a059b13e0eeec8abc77926b3915
KangSK-KAIST <steelyk7@gmail.com>
2021-03-08 09:25:57 +0900

    Moved source.list version to <ver>.2 Fixed the make.sh file correspondingly

--- Dockerfiles/debian/make.sh
+++ Dockerfiles/debian/make.sh
@@ -2,3 +2,7 @@ cd 10.1
 docker build --tag kangsk/debian:10.1 .
 cd ../9.1
 docker build --tag kangsk/debian:9.1 .
+cd ../10.2
+docker build --tag kangsk/debian:10.2 .
+cd ../9.2
+docker build --tag kangsk/debian:9.2 .
